0969cd0436bd2014d07c790f3a7bb8f581e62838
Sayantan <maitysayantan111@gmail.com>
2023-07-14 14:11:46 +0530

    completed upto object

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1.ways  to print javascript
+// console.log("Hello world");
+// document.write("this is javascript");
+// alert("me");
+
+//2.JavaScript console API
+// console.log("Hello world", 4 + 6, "another log");
+// console.warn("this is warning");
+// console.error("This is an error");
+
+//3. JavaScript Variables
+//What are Variables? - containers to store data  values
+var number1 = 34;
+var number2 = 56;
+console.log(number1+number2);
+
+// 4.Data Types in JavaScript
+var num1 = 455;
+var num2 = 56.76;
+
+//string
+var str1 = "This is a string";
+var str2 = "This is also a string";
+
+// objects
+var marks = {
+    ravi:34,
+    subham:78,
+    harry:99.997
+}
+console.log(marks);
